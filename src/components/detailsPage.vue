1c53d788c79aa8f872ff328e8474b5befc3ec8a3
Neskoff <neskoff@gmail.com>
2021-08-16 19:00:00 +0200

    Implement Material Design #6

---
+++ src/components/detailsPage.vue
@@ -0,0 +1,29 @@
+<script>
+import { Line } from "vue3-chart-v2";
+const config = {
+  innerWidth: 400,
+  innerHeight: 400,
+  type: "line",
+  data: data,
+};
+const labels = [1, 2, 3, 4, 5, 6, 7];
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+    },
+  ],
+};
+export default {
+  name: "detailsPage",
+  extends: Line,
+  mounted() {
+    this.renderChart(data, config);
+  },
+};
+</script>
